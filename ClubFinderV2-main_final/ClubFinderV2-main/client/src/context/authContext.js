26b24a3b0d16e92988d0e5d918643d68a3469ead
Janet Tsegazeab <105993774+Janet-T1@users.noreply.github.com>
2025-03-10 15:04:12 +0000

    auth api route updated

--- ClubFinderV2-main_final/ClubFinderV2-main/client/src/context/authContext.js
+++ ClubFinderV2-main_final/ClubFinderV2-main/client/src/context/authContext.js
@@ -9,7 +9,7 @@ export const AuthContextProvider = ({ children }) => {
     );
 
     const login = async (inputs) => {
-        const res = await axios.post("http://localhost:8800/api/auth/login", inputs, {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, inputs, {
             withCredentials: true,
         });
         setCurrentUser(res.data);
